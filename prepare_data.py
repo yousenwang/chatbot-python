9b39937c8f80e732050132412f5ddfc44cd30e45
Ethan <ethan.ys.wang@fii-foxconn.com>
2023-03-21 11:41:29 +0800

    add model details

--- prepare_data.py
+++ prepare_data.py
@@ -81,6 +81,31 @@ from tensorflow.keras.optimizers import SGD
 
 
 model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(
+    learning_rate=0.01,
+    momentum=0.9,
+    nesterov=True
+)
+
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=sgd,
+    metrics=['accuracy']
+)
+
+fitted_model = model.fit(
+    np.array(train_x),
+    np.array(train_y),
+    epochs=200,
+    batch_size=5,
+    verbose=1
+)
 
 
-exit()
+model.save('chatbot_model.h5', fitted_model)
